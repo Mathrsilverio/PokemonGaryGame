26f5c43babf1da6a18a08feba3e05a33357fa9e5
Matazzo <mathrsilverio@gmail.com>
2022-09-21 23:58:12 -0300

    new file:   .github/Preview.png 	new file:   README.md 	new file:   assets/001.png 	new file:   assets/004.png 	new file:   assets/007.png 	new file:   assets/Background.png 	new file:   assets/Bulbasaur.jpg 	new file:   assets/Charmander.jpg 	new file:   assets/Gary.png 	new file:   assets/Squirtle.jpg 	new file:   assets/logo-pokemon.png 	new file:   assets/pokeball.png 	new file:   index.html 	new file:   script.js 	new file:   styles.css

---
+++ README.md
@@ -0,0 +1,29 @@
+# Pokémon! Desafio de Gary (Gary's Challenge)
+
+![preview](./.github/Preview.png)
+
+> Pokémon!
+
+Esse é um jogo de pedra, papel e tesoura, mas com os Pokémons da primeira geração. 
+Faça sua escolha e tente vencer Gary! Ele irá escolher aleatoriamente entre Bulbassauro, Charmander e Squitle. Lembre-se:
+- Fogo vence planta;
+- Água vence fogo;
+- Planta vence água.
+
+*It's a rock, paper, scissors game, but with the initials from Gen I of Pokémon! Make your choice and yry to beat Gary! He will choose randomly between Bulbasaur, Charmander and Squirtle. Remember:*
+*- Fire beats plant;*
+*- Water beats fire;*
+*- Plant beats water;*
+
+[Clique aqui para acessar](https://matazzo.github.io/)
+
+## Tecnologias
+
+- HTML
+- CSS
+- Javascript
+- Git e Github
+
+## Contato
+
+mathrsilverio@gmail.com
---
+++ script.js
@@ -0,0 +1,68 @@
+
+var pokemons = ["Bulbasaur", "Charmander", "Squirtle"];
+
+function obtemPokemonSelecionado() {
+  var radioEscolhas = document.getElementsByName("escolha");
+  for (var i = 0; i < radioEscolhas.length; i++) {
+    if (radioEscolhas[i].checked == true) {
+      return radioEscolhas[i].value;
+    }
+  }
+}
+
+function jogar() {
+  var pokemonSelecionado = obtemPokemonSelecionado();
+  sortearPokemon();
+  var elementoResultado = document.getElementById("resultado");
+  var elementoEscolhaGary = document.getElementById("escolhaGary");
+  if (pokemonSelecionado === pokemons[0] && pokemonGary === pokemons[2]) {
+    elementoResultado.innerHTML = "Você venceu!";
+    elementoEscolhaGary.innerHTML =
+      '<img src="./assets/007.png" style="width:60px;height:60px" alt="Imagem do Squirtle">';
+  }
+  if (pokemonSelecionado === pokemons[0] && pokemonGary === pokemons[1]) {
+    elementoResultado.innerHTML = "Você perdeu!";
+    elementoEscolhaGary.innerHTML =
+      '<img src="./assets/004.png" style="width:60px;height:60px" alt="Imagem do Charmander">';
+  }
+  if (pokemonSelecionado === pokemons[0] && pokemonGary === pokemons[0]) {
+    elementoResultado.innerHTML = "É um empate!";
+    elementoEscolhaGary.innerHTML =
+      '<img src="./assets/001.png " style="width:60px;height:60px" alt="Imagem do Bulbasaur">';
+  }
+  if (pokemonSelecionado === pokemons[1] && pokemonGary === pokemons[0]) {
+    elementoResultado.innerHTML = "Você venceu!";
+    elementoEscolhaGary.innerHTML =
+      '<img src="./assets/001.png" style="width:60px;height:60px" alt="Imagem do Bulbasaur">';
+  }
+  if (pokemonSelecionado === pokemons[1] && pokemonGary === pokemons[2]) {
+    elementoResultado.innerHTML = "Você perdeu!";
+    elementoEscolhaGary.innerHTML =
+      '<img src="./assets/007.png" style="width:60px;height:60px" alt="Imagem do Squirtle">';
+  }
+  if (pokemonSelecionado === pokemons[1] && pokemonGary === pokemons[1]) {
+    elementoResultado.innerHTML = "É um empate!";
+    elementoEscolhaGary.innerHTML =
+      '<img src="./assets/004.png" style="width:60px;height:60px" alt="Imagem do Charmander">';
+  }
+  if (pokemonSelecionado === pokemons[2] && pokemonGary === pokemons[1]) {
+    elementoResultado.innerHTML = "Você venceu!";
+    elementoEscolhaGary.innerHTML =
+      '<img src="./assets/004.png" style="width:60px;height:60px" alt="Imagem do Charmander">';
+  }
+  if (pokemonSelecionado === pokemons[2] && pokemonGary === pokemons[0]) {
+    elementoResultado.innerHTML = "Você perdeu!";
+    elementoEscolhaGary.innerHTML =
+      '<img src="./assets/001.png" style="width:60px;height:60px" alt="Imagem do Bulbasaur">';
+  }
+  if (pokemonSelecionado === pokemons[2] && pokemonGary === pokemons[2]) {
+    elementoResultado.innerHTML = "É um empate!";
+    elementoEscolhaGary.innerHTML =
+      '<img src="./assets/007.png" style="width:60px;height:60px" alt="Imagem do Squirtle">';
+  }
+}
+
+function sortearPokemon() {
+  var numeroPokemonGary = parseInt(Math.random() * 3);
+  pokemonGary = pokemons[numeroPokemonGary];
+}
